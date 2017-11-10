5ed9017505cd9f48f52333062737aafb2d2e9ba9
JessyCuevas <jessy.cuevas.aranda@gmail.com>
2017-11-09 18:29:43 -0600

    práctica-banderas

---
+++ views/parte-cuatro/js/app.js
@@ -0,0 +1,23 @@
+
+	.one{
+		width: 100%; /*largo*/
+		height: 100px;/* ancho */
+		display: inline-block;
+		margin-top: 10px;
+		background: #f7ba0d;
+	}
+	.two{
+		width: 100%; /*largo*/
+		height: 100px;/* ancho */
+		display: inline-block;
+		margin-top: 10px;
+		background: #1d1b24;
+		color: #fff;
+	}
+	.three{
+		width: 100%; /*largo*/
+		height: 100px;/* ancho */
+		display: inline-block;
+		margin-top: 10px;
+		background: #f7ba0d;
+	}
